8810fb99894273af4d89d7632786044f7bc08022
MaxPayne8 <maxpayne0852.com>
2023-10-09 22:03:34 +0530

    bug_fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const YOUTUBE_API =
   API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
