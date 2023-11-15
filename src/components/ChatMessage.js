2c842112744a721ab5a1fab793e256f5b3e87a6d
MaxPayne8 <maxpayne0852.com>
2023-11-15 22:03:59 +0530

    responsive

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatMessage = ({ name, message }) => {
   return (
-    <div className="flex bg-slate-200 mb-1">
+    <div className="flex bg-slate-200 mb-1 w-screen md:w-auto">
       <img
         className="w-8 h-8"
         src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
