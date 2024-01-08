4a56e7b73e4131310efc501a0ed3f9cdafc9a373
Zatin Pandey <109643900+MaxPayne8@users.noreply.github.com>
2024-01-09 02:05:16 +0530

    Update SearchedVideo.js

--- src/components/SearchedVideo.js
+++ src/components/SearchedVideo.js
@@ -41,12 +41,7 @@ const SearchedVideo = () => {
           )}
         </div>
       )}
-      <button
-        onClick={() => window.scrollTo(0, 0)}
-        className="px-3 relative flex justify-center  rounded-lg mx-auto  text-white bg-violet-700 font-semibold hover:bg-violet-900  mt-1"
-      >
-        TOP
-      </button>
+     
     </div>
   );
 };
